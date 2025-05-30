abdf13a52828b7ea09a77500f9680911618b6618
adhikari.gokul5 <adhikari.gokul5@gmail.com>
2025-05-30 10:20:08 +0000

    firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Header';
+import {Header} from '../components/Header';
 import Hero from '../components/Hero';
 import EcosystemSection from '../components/EcosystemSection';
 import DistinctiveDesign from '../components/DistinctiveDesign';
